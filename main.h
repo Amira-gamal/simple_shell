ce97ea8b6bcb281a88c20008cffb51d07cc293aa
Amira-gamal <amiragamal7814@gmail.com>
2023-12-13 13:31:51 -0800

    asdf

--- main.h
+++ main.h
@@ -17,22 +17,23 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* _str_.c */
-int _slen(char *);
-int _scmp(char *, char *);
-char *start(const char *, const char *);
-char *_str_cat(char *, char *);
+/* string_functions.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
 
-/* _str_2.c */
-char *_scpy(char *, char *);
-char *_sdup(const char *);
-void _put(char *);
+/* string_functions2.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
 int _putchar(char);
 
-/* _str_3.c  */
-char *_sncpy(char *, char *, int);
-char *_sncat(char *, char *, int);
-char *_schr(char *, char);
+/* string_functions3.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
 
-
-#endif
+/* string_functions4.c */
+char **strtow(char *, char *);
+char **strtow2(char *, char);
